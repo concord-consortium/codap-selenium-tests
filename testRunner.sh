ca39d07e56f25afa1311ea13d0ad4ef8e36a4b5c
Evangeline Ireland <eireland@concord.org>
2015-07-29 13:46:17 -0700

    test push

--- testRunner.sh
+++ testRunner.sh
@@ -1,30 +1,29 @@
 #! /bin/bash
-
-RESULT_FILE="testrun.csv"
+RESULT_FILE="testruntests.csv"
 TRIALNUM=5
-CASESNUM=20
-TESTNUM=3
-BROWSER=("chrome", "firefox")
-i=1
-b=1
-printf "Browser\t Test No.\t Trials\t Cases\t Duration\t Duration(s)\n" >> $RESULT_FILE
+CASESNUM=10
+TESTNUM=5
+BROWSER=("chrome" "firefox")
+i=0
+b=0
+printf "Time\t Browser\t Test No.\t Trials\t Cases\t Duration\t Duration(s)\n" >> $RESULT_FILE
 tLen=${#BROWSER[@]}
-while [ $i -le $TESTNUM ]
+printf "tLen is ${tLen}"
+while [ $i -le $TESTNUM ];
 do
-    while [ $b -le $tLen ]
-        do
-            START=$(date)
-            START_SEC=$(date +"%s")
-            ruby testPerformanceHarness.rb -t $TRIALNUM -c $CASESNUM -b ${BROWSER[$b]}
-            END=$(date)
-            END_SEC=$(date +"%s")
-            DURATION=$(($END_SEC-$START_SEC))
-            echo "$BROWSER Test $i Time end: $END    TrialNum: $TRIALNUM CasesNum: $CASESNUM Duration: $(($DURATION / 60))m $(($DURATION % 60))s $DURATION s"
-            printf "$BROWSER\t $i\t $TRIALNUM\t $CASESNUM\t  $(($DURATION / 60))m $(($DURATION % 60))s\t $DURATION\n" >> $RESULT_FILE
-            b=$(($b+1))
-        done
-    START=$(date)
-    START_SEC=$(date +"%s")
-
-    i=$(($i+1))
+   for  $b -le $tLen ];
+    do
+        START=$(date)
+        START_SEC=$(date +"%s")
+        ruby testPerformanceHarness.rb -t $TRIALNUM -c $CASESNUM -b ${BROWSER[$b]} -r codap.concord.org/releases/ -v latest
+        END=$(date)
+        END_SEC=$(date +"%s")
+        DURATION=$(($END_SEC-$START_SEC))
+        echo " Time end: $END    ${BROWSER[$b]} Test $i TrialNum: $TRIALNUM CaseNum; $CASESNUM Duration: $(($DURATION / 60))m $((DURATION % 60))s $DURATION s"
+        printf "$END\t ${BROWSER[$b]}\t $i\t $TRIALNUM\t $CASESNUM\t  $(($DURATION / 60))m $((DURATION % 60))s\t $DURATION\n" >> $RESULT_FILE
+        ((b++))
+        printf "b=${b}"
+    done
+((i++))
+printf "i=${i}"
 done
