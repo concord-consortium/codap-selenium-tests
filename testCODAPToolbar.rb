303a9ccc3d078656e888f60da779f4f384dab5c7
Evangeline Ireland <eireland@concord.org>
2015-12-08 21:58:23 -0800

    Simplified code for multiple browser testing and added code for returning browser logs

--- testCODAPToolbar.rb
+++ testCODAPToolbar.rb
@@ -2,48 +2,69 @@ require 'selenium-webdriver'
 require 'rspec/expectations'
 require './codap_object'
 include RSpec::Matchers
+require './LogReporter'
 
-
-def setupMac(browser_name)
-  @driver = Selenium::WebDriver.for browser_name
-  ENV['base_url'] = 'http://codap.concord.org/~eireland/CodapClasses/'
-rescue Exception => e
-  puts e.message
-  puts "Could not start driver"
-  exit 1
-
+def setupHelper(session_id)
+  @logger = LogReporter.new(session_id)
 end
 
-def setupWin(browser_name)
+def setup(browser_name, platform)
+  caps = Selenium::WebDriver::Remote::Capabilities.new
+  puts "Before case, browser name is #{browser_name}, platform name is #{platform}"
+  case (platform)
+    when "windows"
+      caps[:platform] = "Windows"
+      case (browser_name)
+        when :firefox
+          caps[:browserName] = :firefox
+        when :chrome
+          caps[:browserName] = :chrome
+        when :ie
+          caps[:browserName] = 'internet explorer'
+      end
+    when "mac"
+      caps[:platform] = 'OS X'
+      case (browser_name)
+        when :firefox
+          caps[:browserName] = :firefox
+        when :chrome
+          caps[:browserName] = :chrome
+        when :safari
+          caps[:browserName] = :safari
+      end
+  end
   @driver = Selenium::WebDriver.for(
       :remote,
       :url=> 'http://localhost:4444/wd/hub',
-      :desired_capabilities=> browser_name)
+      :desired_capabilities=> caps )
+  setupHelper(@driver.session_id)
   ENV['base_url'] = 'http://codap.concord.org/~eireland/CodapClasses'
+  puts "platform is #{@driver.capabilities.platform}, browser is #{@driver.capabilities.browser_name}"
 rescue Exception => e
   puts e.message
   puts "Could not start driver #{@browser_name}"
   exit 1
 end
 
+
 def teardown
   puts "in teardown"
   @driver.quit
 end
 
-MACBROWSERS = [:firefox, :chrome, :safari]
+MACBROWSERS = [:firefox, :chrome]
 WINBROWSERS = [:firefox, :chrome, :ie]
 
 def run
   MACBROWSERS.each do |macbrowser|
     puts macbrowser
-    setupMac(macbrowser)
+    setup(macbrowser, "mac")
     yield
     teardown
   end
   WINBROWSERS.each do |winbrowser|
     puts winbrowser
-    setupWin(winbrowser)
+    setup(winbrowser, "windows")
     yield
     teardown
   end
@@ -51,6 +72,7 @@ end
 
 run do
   codap = CODAPObject.new(@driver)
+  codap.start_new_doc
   codap.click_table_button
   codap.click_graph_button
   codap.click_map_button
@@ -60,4 +82,5 @@ run do
   codap.click_option_button
   codap.click_tilelist_button
   #codap.click_guide_button
+  puts @logger.latest if @driver.capabilities.browser_name !='internet explorer'
 end
