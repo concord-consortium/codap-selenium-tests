4e461b2179d6edb36827a55608e95ff5408d6563
Evangeline Ireland <eireland@concord.org>
2016-12-06 15:44:14 -0800

    Added functionality for testing graph palette tool ruler.

--- graph_object.rb
+++ graph_object.rb
@@ -15,23 +15,28 @@ module GraphObject
   GRAPH_SCREENSHOT = {css: '.display-camera'}
   GRAPH_COUNT = {css: '.graph-count-check'}
   GRAPH_PERCENT = {css: '.graph-percent-check'}
+  GRAPH_PERCENT_ROW = {css: '.radio-0'}
+  GRAPH_PERCENT_COLUMN = {css: '.radio-1'}
+  GRAPH_PERCENT_CELL = {css: '.radio-2'}
   GRAPH_CONNECTING_LINE = {css: '.graph-connectingLine-check'}
   GRAPH_MOVABLE_VALUE = {css: '.graph-movableValue-check'}
   GRAPH_MOVABLE_LINE = {css: '.graph-movableLine-check'}
+  GRAPH_LSR_LINE = {css: '.graph-lsrl-check'}
   GRAPH_INTERCEPT_LOCKED = {css: '.graph-interceptLocked-check'}
-  GRAPH_SQUARES = {css: '.graph-squares-check'}
   GRAPH_MEAN = {css: '.graph-plottedMean-check'}
   GRAPH_MEDIAN = {css: '.graph-plottedMedian-check'}
   GRAPH_STD_DEV = {css: '.graph-plottedStdDev-check'}
   GRAPH_INTERQUARTILE = {css: '.graph-plottedIQR-check'}
   GRAPH_PLOTTED_VALUE = {css: '.graph-plottedValue-check'}
   GRAPH_PLOTTED_FUNCTION = {css: '.graph-plottedFunction-check'}
+  GRAPH_SQUARES = {css: '.graph-squares-check'}
+  GRAPH_BOX_PLOT = {css: '.graph-plottedBoxPlot-check'}
   GRAPH_POINT_SIZE_SLIDER = {css: '.graph-pointSize-slider'}
   GRAPH_POINT_COLOR_PICKER = {css: '.graph-point-color'}
   GRAPH_STROKE_COLOR_PICKER = {css: '.graph-stroke-color'}
   GRAPH_TRANSPARENT = {css: '.graph-transparent-check'}
   GRAPH_SCREENSHOT_DOWNLOAD = {css: '.buttons>a'}
-  GRPAH_SCREENSHOT_INPUT_FILENAME = {css: '.localFileSave>input'}
+  GRAPH_SCREENSHOT_INPUT_FILENAME = {css: '.localFileSave>input'}
   GRAPH_SCREENSHOT_DIALOG_TITLE = {css: '.modal-dialog-title'}
 
   SINGLE_TEXT_DIALOG_TEXTFIELD = {css: '.dg-single-text-dialog-textfield'}
@@ -62,21 +67,108 @@ module GraphObject
   end
 
   def take_screenshot(attribute,location)
+    puts "in graph screenshot"
     click_on(GRAPH_TILE)
+    puts "clicked on graph tile"
     click_on(GRAPH_SCREENSHOT)
+    puts "clicked on graph_screenshot"
     wait_for{ displayed?(GRAPH_SCREENSHOT_DIALOG_TITLE) }
-     screenshot_popup = find(GRPAH_SCREENSHOT_INPUT_FILENAME)
+     screenshot_popup = find(GRAPH_SCREENSHOT_INPUT_FILENAME)
      puts "Found screenshot_popup at #{screenshot_popup}"
-     screenshot_filename = "graph_#{attribute}_on_#{location}" #add a specific folder to save snapshots to
+     screenshot_filename = "graph_#{attribute}_on_#{location} count" #add a specific folder to save snapshots to
      screenshot_popup.clear
      screenshot_popup.send_keys(screenshot_filename)
+    sleep(3)
+    wait_for{ displayed?(GRAPH_SCREENSHOT_DOWNLOAD)}
+    download_button = find(GRAPH_SCREENSHOT_DOWNLOAD)
+    puts "found download button at #{download_button}"
     click_on(GRAPH_SCREENSHOT_DOWNLOAD)
+    puts "clicked on download button"
+    sleep(3)
   end
 
   def turn_on_count()
+    puts 'in turn_on_count'
       click_on(GRAPH_TILE)
       click_on(GRAPH_RULER)
-      click_on(GRAPH_COUNT)
+    wait_for{ displayed?(GRAPH_COUNT)}
+    sleep(1)
+    click_on(GRAPH_COUNT)
+    puts "clicked on graph count #{GRAPH_COUNT}"
   end
 
+  def turn_off_count()
+    puts 'in turn_on_count'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_COUNT)}
+    sleep(1)
+    click_on(GRAPH_COUNT)
+    puts "clicked on graph count #{GRAPH_COUNT}"
+  end
+
+  def turn_on_percent()
+    puts 'in turn_on_percent'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_PERCENT)}
+    sleep(1)
+    click_on(GRAPH_PERCENT)
+    puts "clicked on graph percent #{GRAPH_PERCENT}"
+  end
+
+  def turn_off_percent()
+    puts 'in turn_on_percent'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_PERCENT)}
+    sleep(1)
+    click_on(GRAPH_PERCENT)
+    puts "clicked on graph percent #{GRAPH_PERCENT}"
+  end
+
+  def show_percent_column()
+    puts 'in show_percent_column'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_PERCENT)}
+    percent_checkbox = find(GRAPH_PERCENT)
+    sleep(1)
+    if (percent_checkbox.attribute('aria-checked') == 'false')
+      click_on(GRAPH_PERCENT)
+      puts "clicked on graph percent #{GRAPH_PERCENT}"
+    end
+    click_on(GRAPH_PERCENT_COLUMN)
+    puts "clicked on graph percent column #{GRAPH_PERCENT_COLUMN}"
+  end
+
+  def show_percent_cell()
+    puts 'in turn_on_percent'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_PERCENT)}
+    percent_checkbox = find(GRAPH_PERCENT)
+    sleep(1)
+    if (percent_checkbox.attribute('aria-checked') == 'false')
+      click_on(GRAPH_PERCENT)
+      puts "clicked on graph percent #{GRAPH_PERCENT}"
+    end
+    click_on(GRAPH_PERCENT_CELL)
+    puts "clicked on graph percent column #{GRAPH_PERCENT_CELL}"
+  end
+
+  def show_percent_row()
+    puts 'in turn_on_percent'
+    click_on(GRAPH_TILE)
+    click_on(GRAPH_RULER)
+    wait_for{ displayed?(GRAPH_PERCENT)}
+    percent_checkbox = find(GRAPH_PERCENT)
+    sleep(1)
+    if (percent_checkbox.attribute('aria-checked') == 'false')
+      click_on(GRAPH_PERCENT)
+      puts "clicked on graph percent #{GRAPH_PERCENT}"
+    end
+    click_on(GRAPH_PERCENT_ROW)
+    puts "clicked on graph percent column #{GRAPH_PERCENT_ROW}"
+  end
 end
---
+++ testGraphToolPalette.rb
@@ -0,0 +1,147 @@
+#testGraphToolPalette test graph tool palette values transitions and take graph screenshots of each transition. The data file 3TableGroups.json need to be in the same directory as the test. Plot transitions screenshots are saved separately by plot transition name. The test will run on latest version of CODAP in Mac OS X Chrome, Firefox and Safari, and Windows 8.1 Chrome, Firefox and Internet Explorer 11.
+
+require './codap_object'
+
+
+def write_result_file(doc_name)
+  googledrive_path="Google Drive/CODAP @ Concord/Software Development/QA"
+  localdrive_path="Documents/CODAP data/"
+  $dir_path = "Documents/CODAP data/"
+  $save_filename = "Plot_changes_logs"
+
+  log = @@driver.manage.logs.get(:browser)
+  messages = ""
+  log.each {|item| messages += item.message + "\n"}
+
+  if !File.exist?("#{Dir.home}/#{$dir_path}/#{$save_filename}") || $new_file
+    File.open("#{Dir.home}/#{$dir_path}/#{$save_filename}", "wb") do |log|
+      log<< messages unless messages == ""
+    end
+  else
+    File.open("#{Dir.home}/#{$dir_path}/#{$save_filename}", "a") do |log|
+      log << messages unless messages == ""
+    end
+  end
+
+end
+
+def test_ruler_tool()
+  codap.drag_attribute('ACAT2', 'y')
+  sleep(2)
+  codap.turn_on_count
+  sleep(3)
+  codap.take_screenshot('ACAT2','count')
+  codap.turn_on_percent
+  sleep(3)
+  codap.take_screenshot('ACAT2','countpercent')
+  codap.turn_off_count
+  sleep(3)
+  codap.take_screenshot('ACAT2','percent')
+  codap.turn_off_percent
+
+  codap.drag_attribute('ACAT1','x')
+  sleep(2)
+  codap.turn_on_count
+  sleep(3)
+  codap.take_screenshot('ACAT1x','count')
+  codap.turn_on_percent
+  sleep(3)
+  codap.take_screenshot('ACAT1x','countpercent')
+  sleep(3)
+  codap.take_screenshot('ACAT1x','percent_row')
+  codap.show_percent_column
+  sleep(3)
+  codap.take_screenshot('ACAT1x','percent_column')
+  codap.show_percent_cell
+  sleep(3)
+  codap.take_screenshot('ACAT1x','percent_cell')
+  codap.turn_off_count
+  sleep(3)
+  codap.take_screenshot('ACATx','percent_only')
+  codap.turn_off_percent
+end
+
+codap = CODAPObject.new()
+codap.setup_one(:chrome)
+url = "https://codap.concord.org/releases/staging/"
+open_doc = '3TableGroups.json'
+file = File.absolute_path(File.join(Dir.pwd, open_doc))
+puts "file is #{file}, open_doc is #{open_doc}"
+attributes = ['ACAT1','ACAT2','ANUM1','ANUM2','BCAT1','BNUM1','CCAT1','CNUM1','CCAT2','CNUM2']
+drop_zone = ['x','y','legend']
+array_of_plots = [{:attribute=>'ACAT1', :axis=>'x'},
+                  {:attribute=>'ACAT2', :axis=>'y'},
+                  {:attribute=>'ANUM1', :axis=>'x'},
+                  {:attribute=>'ANUM2', :axis=>'y'},
+                  {:attribute=>'BCAT1', :axis=>'x'},
+                  {:attribute=>'BNUM1', :axis=>'x'},
+                  {:attribute=>'CCAT1', :axis=>'x'},
+                  {:attribute=>'CNUM1', :axis=>'x'},
+                  {:attribute=>'BNUM1', :axis=>'y'},
+                  {:attribute=>'CCAT2', :axis=>'x'},
+                  {:attribute=>'BCAT1', :axis=>'y'},
+                  {:attribute=>'ACAT2', :axis=>'y'},
+                  {:attribute=>'BCAT1', :axis=>'graph_legend'},
+                  {:attribute=>'CNUM1', :axis=>'graph_legend'},
+                  {:attribute=>'CNUM2', :axis=>'y'},]
+
+codap.start_codap(url)
+# Open CODAP document
+codap.user_entry_open_doc
+codap.open_local_doc(file)
+open_doc.slice! '.json'
+codap.verify_doc_title(open_doc)
+
+#Open a graph
+codap.click_button('graph')
+sleep(2)
+
+test_ruler_tool
+
+codap.drag_attribute('ACAT2', 'y')
+sleep(2)
+codap.turn_on_count
+sleep(3)
+codap.take_screenshot('ACAT2','count')
+codap.turn_on_percent
+sleep(3)
+codap.take_screenshot('ACAT2','countpercent')
+codap.turn_off_count
+sleep(3)
+codap.take_screenshot('ACAT2','percent')
+codap.turn_off_percent
+
+codap.drag_attribute('ACAT1','x')
+sleep(2)
+codap.turn_on_count
+sleep(3)
+codap.take_screenshot('ACAT1x','count')
+codap.turn_on_percent
+sleep(3)
+codap.take_screenshot('ACAT1x','countpercent')
+sleep(3)
+codap.take_screenshot('ACAT1x','percent_row')
+codap.show_percent_column
+sleep(3)
+codap.take_screenshot('ACAT1x','percent_column')
+codap.show_percent_cell
+sleep(3)
+codap.take_screenshot('ACAT1x','percent_cell')
+codap.turn_off_count
+sleep(3)
+codap.take_screenshot('ACATx','percent_only')
+codap.turn_off_percent
+
+# #Change axes by attribute, axis
+# array_of_plots.each do |hash|
+#   codap.drag_attribute(hash[:attribute], hash[:axis])
+#   sleep(5)
+#   codap.write_log_file('./', open_doc)
+#   codap.take_screenshot(hash[:attribute],hash[:axis])
+# end
+
+# codap.remove_graph_attribute('graph_legend')
+# codap.teardown
+
+# `mkdir -p ~/Sites/plot_transition_results`
+# `mv ~/Downloads/graph_*.png ~/Sites/plot_transition_results/`
