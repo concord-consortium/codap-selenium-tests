bc7b6ce6f86c8833d95e5a50730b21d5df879624
Evangeline Ireland <eireland@concord.org>
2017-01-27 12:10:34 -0800

    Changed the way plugin pages are launched. Plugin pages are now launched using the links in the CODAP Data Interactive Plugin page.

--- testPageLoads.rb
+++ testPageLoads.rb
@@ -1,24 +1,61 @@
 require './codap_object.rb'
 
-screenshot_dir = "#{Dir.home}/Sites/doc_screenshots/"
+LISTING = {css: ".listing-title"}
 
-File.open('CODAPDocLinks.txt').each do |url|
+screenshot_dir = "#{Dir.home}/Sites/doc_screenshots/"
+`mkdir -p #{screenshot_dir}`
+
+# TODO change script so it loads CODAP Sample docs and CODAP Plug-in Page and clicks on each link
+
+def fixPageSourceName(url)
+
+end
+
+def openMiscLinks(screenshot_dir)
+  File.open('CODAPDocLinks.txt').each do |url|
+    codap = CODAPObject.new()
+    codap.setup_one(:chrome)
+    codap.visit(url)
+    # TODO if url fails to load, try again max 5 times.
+    puts "URL is #{url}"
+    sleep(30)
+    if url.include? "#"
+      page_source = url.split('#').last
+    else
+      page_source = url.split('?').last
+    end
+    page_source = page_source.gsub(/[.:=%\/]/, "_").chop
+    puts "Page_source is #{page_source}"
+    # Prepare screenshots dir
+    codap.save_screenshot(screenshot_dir, page_source)
+    codap.write_log_file(screenshot_dir, page_source)
+    codap.teardown
+  end
+end
+
+def openPluginLinks(screenshot_dir)
+  pluginURL = "https://concord-consortium.github.io/codap-data-interactives/"
+
   codap = CODAPObject.new()
   codap.setup_one(:chrome)
-  codap.visit(url)
-  # TODO if url fails to load, try again max 5 times.
-  puts "URL is #{url}"
-  sleep(30)
-  if url.include? "#"
-    page_source = url.split('#').last
+  codap.visit(pluginURL)
+  plugins = codap.find_all(LISTING)
+  if plugins.length>0
+    plugins.each do |plugin|
+      puts "Plugin test is #{plugin.text}"
+    plugin.click
+    sleep(15)
+    codap.save_screenshot(screenshot_dir, plugin.text) # Take screenshot
+    codap.write_log_file(screenshot_dir, plugin.text) # Write log file
+    codap.switch_to_main()
+    end
   else
-    page_source = url.split('?').last
+    puts "No plugins in the list"
   end
-  page_source = page_source.gsub(/[.:=%\/]/, "_").chop
-  puts "Page_source is #{page_source}"
-  # Prepare screenshots dir
-  `mkdir -p #{screenshot_dir}`
-  codap.save_screenshot(screenshot_dir, page_source)
-  codap.write_log_file(screenshot_dir, page_source)
+
   codap.teardown
- end
+
+end
+
+# openMiscLinks(screenshot_dir)
+openPluginLinks(screenshot_dir)
