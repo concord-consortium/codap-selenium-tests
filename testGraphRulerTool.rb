6e61476a86fac49c4b211d1bb49e83f98f39ab4d
Evangeline Ireland <eireland@concord.org>
2017-05-08 15:45:13 -0700

    Added code to close graph so that plot value can be tested again after plotted function has been enabled.

--- testGraphRulerTool.rb
+++ testGraphRulerTool.rb
@@ -2,12 +2,18 @@
 
 require './codap_object'
 
+expected_screenshots_dir = "#{Dir.home}/Sites/plot_ruler_results/expected_screenshots/"
+staging_screenshots_dir = "#{Dir.home}/Sites/plot_ruler_results/test_screenshots/"
+prev_attr = ""
+prev_axis = ""
+$close_graph = false
+
 
 def write_result_file(doc_name)
   googledrive_path="Google Drive/CODAP @ Concord/Software Development/QA"
   localdrive_path="Documents/CODAP data/"
   $dir_path = "Documents/CODAP data/"
-  $save_filename = "Plot_changes_logs"
+  $save_filename = "Plot_ruler_logs"
 
   log = @@driver.manage.logs.get(:browser)
   messages = ""
@@ -28,7 +34,6 @@ end
 CHECKBOX_LOCATOR = {css: '.checkbox'}
 INPUT_FIELD_LOCATOR = {css: "input.field"}
 LABEL_LOCATOR = {xpath: '//input[contains(@class,"field")]/ancestor::div/div[contains(@class, "sc-label-view")]'}
-# GRAPH_TITLE_BAR = {xpath: '//div[contains(@class,"graph-view")]/ancestor::dg/div[contains(@class,"titlebar")]'}
 GRAPH_CLOSE = {css: ".dg-close-view"}
 GRAPH_TITLE_BAR = {css: '.titlebar-selected'}
 GRAPH_TILE = {css: '.graph-view'}
@@ -38,7 +43,6 @@ def click_on_checkboxes(kcodap, state)
 #Turn on checkboxes
   plotted_value = false
   plotted_function = false
-  input = ["x*x/10", "75"]
   input_field_list=[]
 
   kcodap.open_ruler_tool
@@ -56,84 +60,111 @@ def click_on_checkboxes(kcodap, state)
     if checkbox.text == 'Plotted Function'
       plotted_function = true
     end
+
+    if plotted_value && plotted_function
+      $close_graph = true;
+    end
     checkbox_texts +=checkbox.text
     (checkbox).click
     i -=1
-    # sleep(8)
-    # if i!=0
-    #   codap.open_ruler_tool
-    # end
   end
   if state == 'on'
     index=0
     input_field_list = kcodap.find_all(INPUT_FIELD_LOCATOR)
-    if input_field_list.length>0
-      puts "input field list length is #{input_field_list.length}. input field list is #{input_field_list}"
-      input_field_list.each do |input_field|
-        # label_loc = {xpath: '/ancestor::div/div[contains(@class, "sc-label-view")]'}
-        label = kcodap.find(LABEL_LOCATOR)
-        puts "label is #{label.text}"
-        # input_field.send_keys(input[index])
-        index +=1
-      end
+    label_list = kcodap.find_all(LABEL_LOCATOR)
+    while i<input_field_list.length
+        # label_list.each do |label|
+          puts "label is #{label_list[i].text}"
+          input_field_list[i].clear
+          case (label_list[i].text)
+            when "value ="
+              input_field_list[i].send_keys('10')
+            when "y ="
+              input_field_list[i].send_keys('x*x/10')
+          end
+        # end
+        i +=1
+
     end
-    # if plotted_value == true
-    #   puts "plotted value is on"
-    #   input_value_field = kcodap.find(PLOTTED_INPUT_FIELD)
-    #   puts "input_value_field is #{input_value_field}"
-    #   sleep(3)
-    #   kcodap.click_on(PLOTTED_INPUT_FIELD)
-    #   input_value_field.send_keys("75")
-    #   sleep(2)
-    # end
-    # if plotted_function ==true
-    #   input_function_field = kcodap.find(PLOTTED_FUNCTION_FIELD)
-    #   if (input_function_field.text=='')
-    #     input_function_field.send_keys('x*x/10')
-    #     sleep(2)
-    #   end
-    # end
   end
   return checkbox_texts
 end
 
+begin
+  codap = CODAPObject.new()
+  codap.setup_one(:chrome)
+  url = "https://codap.concord.org/releases/staging/"
+  open_doc = '3TableGroups.json'
+  file = File.absolute_path(File.join(Dir.pwd, open_doc))
+  puts "file is #{file}, open_doc is #{open_doc}"
 
-codap = CODAPObject.new()
-codap.setup_one(:chrome)
-url = "https://codap.concord.org/releases/staging/"
-open_doc = '3TableGroups.json'
-file = File.absolute_path(File.join(Dir.pwd, open_doc))
-puts "file is #{file}, open_doc is #{open_doc}"
-attributes = ['ACAT1','ACAT2','ANUM1','ANUM2','BCAT1','BNUM1','CCAT1','CNUM1','CCAT2','CNUM2']
-drop_zone = ['x','y','legend']
-array_of_plots = [{:attribute=>'ACAT1', :axis=>'y'},
-                  {:attribute=>'ACAT2', :axis=>'x'},
-                  {:attribute=>'ANUM1', :axis=>'x'},
-                  {:attribute=>'ANUM2', :axis=>'y'},
-                  {:attribute=>'BCAT1', :axis=>'x'},
-                  {:attribute=>'BNUM1', :axis=>'x'},
-                  {:attribute=>'CCAT1', :axis=>'x'},
-                  {:attribute=>'CNUM1', :axis=>'x'},
-                  {:attribute=>'BNUM1', :axis=>'y'},
-                  {:attribute=>'CCAT2', :axis=>'x'},
-                  {:attribute=>'BCAT1', :axis=>'y'},
-                  {:attribute=>'ACAT2', :axis=>'y'},
-                  {:attribute=>'BCAT1', :axis=>'graph_legend'},
-                  {:attribute=>'CNUM1', :axis=>'graph_legend'},
-                  {:attribute=>'CNUM2', :axis=>'y'},]
+  array_of_plots = [{:attribute=>'ACAT1', :axis=>'y'},
+                    {:attribute=>'BCAT1', :axis=>'x'},
+                    {:attribute=>'ANUM1', :axis=>'x'},
+                    {:attribute=>'BNUM1', :axis=>'y'},
+                    {:attribute=>'BNUM1', :axis=>'x'},
+                    {:attribute=>'CCAT1', :axis=>'y'},
+                    {:attribute=>'CNUM1', :axis=>'x'},
+                    {:attribute=>'BNUM1', :axis=>'y'},
+                    {:attribute=>'CCAT2', :axis=>'x'},
+                    {:attribute=>'BCAT1', :axis=>'y'},
+                    {:attribute=>'ACAT2', :axis=>'y'},
+                    {:attribute=>'BCAT1', :axis=>'graph_legend'},
+                    {:attribute=>'CNUM1', :axis=>'graph_legend'},
+                    {:attribute=>'CNUM2', :axis=>'y'}]
 
-codap.start_codap(url)
+  codap.start_codap(url)
 # Open CODAP document
-codap.user_entry_open_doc
-codap.open_local_doc(file)
-sleep(2)
-open_doc.slice! '.json'
-codap.verify_doc_title(open_doc)
+  codap.user_entry_open_doc
+  codap.open_local_doc(file)
+  sleep(2)
+  open_doc.slice! '.json'
+  codap.verify_doc_title(open_doc)
 
 #Open a graph
-codap.click_button('graph')
-sleep(2)
-codap.click_on(GRAPH_TILE)
+  codap.click_button('graph')
+  codap.wait_for{codap.displayed? (GRAPH_TILE) }
+
+#Change axes by attribute, axis
+  array_of_plots.each do |hash|
+    sleep(2)
+    codap.drag_attribute(hash[:attribute], hash[:axis])
+    sleep(2)
+    checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
+    sleep(2)
+    codap.write_log_file('./', open_doc)
+    codap.take_screenshot(hash[:attribute],hash[:axis], prev_attr, prev_axis, checkbox_texts)
+    checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
+    prev_attr = hash[:attribute]
+    prev_axis = hash[:axis]
+
+    #After having both value and function plotted on graph, two input fields are present in DOM even if only one input field is visible. So if both value and function have been plotted, we close the existing graph and open a new one.
+    if $close_graph==true
+      title_bar = codap.find(GRAPH_TITLE_BAR)
+      codap.close_component(title_bar)
+      codap.click_button('graph')
+      codap.wait_for{codap.displayed? (GRAPH_TILE) }
+      $close_graph=false
+    end
+  end
+  codap.teardown
+
+rescue => e
+  puts '::ERROR::'
+  puts e
+  attempt +=1
+  if attempt< max_attempts
+    puts "RETRYING (#{attempt})..."
+    retry
+  end
+end
+
+`rm -rf ~/Sites/plot_ruler_results/test_screenshots`
+`mkdir -p ~/Sites/plot_ruler_results/test_screenshots`
+`mv ~/Downloads/graph_*.png ~/Sites/plot_ruler_results/test_screenshots/`
+
+# size_compare_result = codap.compare_file_sizes(staging_screenshots_dir, expected_screenshots_dir)
+# puts ("#{size_compare_result}")
 
 # codap.drag_attribute('ACAT2', 'x') # Univariate categorical axis
 # sleep(2)
@@ -142,98 +173,37 @@ codap.click_on(GRAPH_TILE)
 # codap.take_screenshot('ACAT2x', checkbox_texts)
 # checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
 #
-# codap.drag_attribute('ACAT1','y')
+# codap.drag_attribute('BCAT1','y') #cat v cat
 # sleep(2)
 # checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
 # sleep(2)
-# codap.take_screenshot('ACAT1y', checkbox_texts)
+# codap.take_screenshot('BCAT1y', checkbox_texts)
 # checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
-
-# codap.drag_attribute('ANUM1','x')
+#
+# codap.drag_attribute('ANUM1','x') #num v cat
 # sleep(2)
 # checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
 # codap.take_screenshot('ANUM1x', checkbox_texts)
 # checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
 #
-# codap.drag_attribute('ANUM2','y')
+# codap.drag_attribute('ANUM2','y') #num v num
 # sleep(2)
 # checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
 # codap.take_screenshot('ANUM2y', checkbox_texts)
 # checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
 # codap.take_screenshot('ANum2y_checkboxoff',checkbox_texts)
-title_bar = codap.find(GRAPH_TITLE_BAR)
-codap.close_component(title_bar)
-
-codap.click_button('graph')
-sleep(2)
-codap.drag_attribute('ANUM1','x')
-sleep(2)
-checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
-codap.take_screenshot('ANUM1x', checkbox_texts)
-checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
-
-codap.remove_graph_attribute('x')
-sleep(2)
-checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
-codap.take_screenshot('ANUM1y', checkbox_texts)
-checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
-codap.take_screenshot('ANum1y_checkboxoff',checkbox_texts)
-
-sleep(5)
-
-#Turn off checkboxes
-
-#for each checkbox in the list, turn the checkbox on, take a snapshot, turn checkbox off, take a snapshot
-#what to do about combination checkboxes ([count and percent], percent with [row,column, and cell], [movable line and lsrl] with [intercept, and squares of residuals])
-# codap.turn_on_count
-# sleep(3)
-# codap.take_screenshot('ACAT2','count')
-# codap.turn_on_percent
-# sleep(3)
-# codap.take_screenshot('ACAT2','countpercent')
-# codap.turn_off_count
-# sleep(3)
-# codap.take_screenshot('ACAT2','percent')
-# codap.turn_off_percent
 #
-# codap.drag_attribute('ACAT1','x')
-# sleep(2)
-# codap.turn_on_count
-# sleep(3)
-# codap.take_screenshot('ACAT1x','count')
-# codap.turn_on_percent
-# sleep(3)
-# codap.take_screenshot('ACAT1x','countpercent')
-# sleep(3)
-# codap.take_screenshot('ACAT1x','percent_row')
-# codap.show_percent_column
-# sleep(3)
-# codap.take_screenshot('ACAT1x','percent_column')
-# codap.show_percent_cell
-# sleep(3)
-# codap.take_screenshot('ACAT1x','percent_cell')
-# codap.turn_off_count
-# sleep(3)
-# codap.take_screenshot('ACATx','percent_only')
-# codap.turn_off_percent
+# #Close graph to be able to just have one text field for plot value formula
+# title_bar = codap.find(GRAPH_TITLE_BAR)
+# codap.close_component(title_bar)
 #
-# codap.drag_attribute('ANUM1', 'x')
+# codap.click_button('graph')
 # sleep(2)
-# codap.turn_on_count
-# sleep(3)
-# codap.take_screenshot('ANUM1x','count')
+# codap.drag_attribute('ANUM1','x') #Univariate numerical axis
+# sleep(2)
+# checkbox_texts = click_on_checkboxes(codap,'on') #Turn on checkboxes
+# codap.take_screenshot('ANUM1x', checkbox_texts)
+# checkbox_texts = click_on_checkboxes(codap,'off') #Turn off checkboxes
+#
+# sleep(5)
 
-
-# #Change axes by attribute, axis
-# array_of_plots.each do |hash|
-#   codap.drag_attribute(hash[:attribute], hash[:axis])
-#   sleep(5)
-#   codap.write_log_file('./', open_doc)
-#   codap.take_screenshot(hash[:attribute],hash[:axis])
-# end
-
-# codap.remove_graph_attribute('graph_legend')
-# codap.teardown
-
-# `mkdir -p ~/Sites/plot_transition_results`
-# `mv ~/Downloads/graph_*.png ~/Sites/plot_transition_results/`
