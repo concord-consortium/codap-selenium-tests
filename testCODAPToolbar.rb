bbf3e5e077c4cf9019a33e6209d30474897c6ca7
evangelineireland <eireland@concord.org>
2018-12-10 09:49:32 -0800

    Adds screenshot to the toolbar test

--- testCODAPToolbar.rb
+++ testCODAPToolbar.rb
@@ -2,6 +2,12 @@
 
 require './codap_object.rb'
 
+$expected_screenshots_dir = "#{Dir.home}/Sites/toolbar_results/expected_screenshots/"
+$test_screenshots_dir = "#{Dir.home}/Sites/toolbar_results/test_screenshots/"
+
+`rm -rf #{$test_screenshots_dir}`
+`mkdir -p #{$test_screenshots_dir}`
+
 url = 'https://codap.concord.org/releases/staging/'
 components = ['table','graph','map','slider','calc','text','sampler','draw tool','option','tilelist', 'help']
 
@@ -13,7 +19,9 @@ sleep(3)
 components.each do |component|
   codap.click_button(component)
   sleep(2)
+  codap.save_screenshot($test_screenshots_dir,"#{component}_screenshot")
 end
+
   #  puts @logger.latest if @driver.capabilities.browser_name !='internet explorer'
-  #TODO Needs assertions for each button click
+  #TODO Needs assertions for each button click - some verification in codap_object.rb
 
